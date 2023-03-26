0c245c3d6b8a1dda9503e56d2511d9affe87e02f
Kyle Eaton <keaton12@Live.com>
2023-03-26 18:38:39 -0500

    Command line args and insertion
    Take in a user id and a rating on the command line and insert a new entry into the Users table (or update an existing entry).

--- app/main.c
+++ app/main.c
@@ -5,16 +5,33 @@
 
 int main(int argc, char *argv[]) {
 	
+	int result;
+	char* error;
+	char insert[100];
 	sqlite3* db;
 
-	int resultCode = sqlite3_open(DB_NAME, &db);
-
-	if(resultCode) {
-		printf("Failed to open database: %s", sqlite3_errmsg(db));
+	if(argc < 3) {
+		printf("Missing arguments:\n");
+		printf("\t./risk [user_id] [rating]\n");
 		return 0;
 	}
 
+	result = sqlite3_open(DB_NAME, &db);
+
+	if(result != SQLITE_OK) {
+		printf("Failed to open database, %d: %s\n", result, sqlite3_errmsg(db));
+		return 0;
+	}
 	printf("Database opened\n");
+
+	sprintf(insert, "INSERT OR REPLACE INTO USERS VALUES(%s, %s);", argv[1], argv[2]);
+	result = sqlite3_exec(db, insert, NULL, NULL, NULL);
+	if(result != SQLITE_OK) {
+		printf("Failed to insert record, %d: %s\n", result, sqlite3_errmsg(db));
+		return 0;
+	}
+	printf("Record inserted\n");
+
 	sqlite3_close(db);	
 	return 0;
 }
